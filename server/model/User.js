7fb0d41801d28f4dfbcf539ba4117b4d565563e8
sonaligadekar20 <sonaliygadekar@gmail.com>
2024-01-05 22:46:08 +0530

    added userSchema & install md5

---
+++ server/model/User.js
@@ -0,0 +1,22 @@
+import {Schema, model} from 'mongoose';
+
+const userSchema = new Schema({
+    email:{
+        type: String,
+        required:[true, 'email is required']
+    },
+    password:{
+        type: String,
+        required:[true, 'password is required']
+    },
+    name: {
+        type:String,
+        required:[true, 'name is required']
+    }
+},{
+    timestamps: true
+}
+);
+
+const user = model('user', userSchema);
+export default user;
