05467b26455eef79a07d9fe680fe4d4b02b399ad
sonaligadekar20 <sonaliygadekar@gmail.com>
2024-01-15 23:36:31 +0530

    routing is added

--- client/src/App.js
+++ client/src/App.js
@@ -1,60 +1,23 @@
-import React, {useEffect, useState} from 'react';
-import { io } from "socket.io-client"
-
-const socket = io("http://localhost:5002")
+import React from 'react'
+import {RouterProvider, createBrowserRouter} from 'react-router-dom'
+import Home from './views/Home/Home.js'
+import Login from './views/Login/Login.js'
 
 function App() {
-  const [message, setMessage] = useState();
-  const [messages, setMessages] = useState();
 
-  /*
-  {
-      sender: 'sender',
-      receiver:'receiver',
-      message: 'message',
-      timestamp: 'timestamp'
-  }
-  */
-    
-  useEffect(() => {
-    socket.on('message', (data) =>{
-      const newMessages = [...messages, data];
-      console.log(newMessages);
-      setMessages(newMessages);
-    });
-  }, []);
-  return (
-    <>
-      <div>
-        <h1>Chat App</h1>
+  const router =createBrowserRouter([
+    {
+      path: '/',
+      element: <Home/>
+    },
+    {
+      path: '/login',
+      element: <Login/>
+    }
+  ])
 
-        <input type = "text" placeholder='Enter messages'
-        onChange={(e) => setMessage (e.target.value)}
-        value={message} />
-
-        {
-          messages.map((message) =>{
-            <div key={message.timestamp}>
-              <p>{message.timestamp}</p>
-              <p>{message.message}</p>
-              <hr/>
-              </div>
-          })
-        }
-        
-        <button onClick={() =>{
-          socket.emit('message',{
-            sender: 'sender',
-            receiver:'receiver',
-            message,
-            timestamp: new Date().toISOString(),
-        });
-        setMessage('');
-        }}> Send Message</button>
-
-      </div>
-    </>
+  return(
+    <RouterProvider router = {router} />
   )
 }
-
 export default App
---
+++ client/src/views/Home/Home.js
@@ -0,0 +1,60 @@
+import React, {useEffect, useState} from 'react';
+import { io } from "socket.io-client"
+
+const socket = io("http://localhost:5002")
+
+function Home() {
+  const [message, setMessage] = useState();
+  const [messages, setMessages] = useState();
+
+  /*
+  {
+      sender: 'sender',
+      receiver:'receiver',
+      message: 'message',
+      timestamp: 'timestamp'
+  }
+  */
+    
+  useEffect(() => {
+    socket.on('message', (data) =>{
+      const newMessages = [...messages, data];
+      console.log(newMessages);
+      setMessages(newMessages);
+    });
+  }, []);
+  return (
+    <>
+      <div>
+        <h1>Chat App</h1>
+
+        <input type = "text" placeholder='Enter messages'
+        onChange={(e) => setMessage (e.target.value)}
+        value={message} />
+
+        {
+          messages.map((message) =>{
+            <div key={message.timestamp}>
+              <p>{message.timestamp}</p>
+              <p>{message.message}</p>
+              <hr/>
+              </div>
+          })
+        }
+        
+        <button onClick={() =>{
+          socket.emit('message',{
+            sender: 'sender',
+            receiver:'receiver',
+            message,
+            timestamp: new Date().toISOString(),
+        });
+        setMessage('');
+        }}> Send Message</button>
+
+      </div>
+    </>
+  )
+}
+
+export default Home
